cbbd46d8e848e5363f9f9a78f8683896b3cc23c9
enth01 <115834672+enth01@users.noreply.github.com>
2022-12-27 21:12:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('btn').onclick = function () {
     x = document.getElementById('x').value
     
     if (x === 'kokot') {
-        window.location.href = "http://127.0.0.1:5555/penis.html";
+        window.location.href = "penis.html";
     } else {
         alert('chod dopice')
     }
